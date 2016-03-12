c0f45321fdc6a4397645e64a1239906d6b368714
EchoEnzo <n8hynes@gmail.com>
2016-03-11 20:30:23 -0330

    Hello World!

--- java/hello/hello.java
+++ java/hello/hello.java
@@ -2,5 +2,6 @@ import java.util.Scanner;
 public class hello {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        System.out.println("Hello World!")
     }
 }
