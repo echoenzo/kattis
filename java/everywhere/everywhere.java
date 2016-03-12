35fb7b76cf3412648f1a422b1de8fe84bd7e9ea3
EchoEnzo <n8hynes@gmail.com>
2016-03-11 21:44:05 -0330

    'I've Been Everywhere, Man'

---
+++ java/everywhere/everywhere.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class everywhere {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int numTrips = in.nextInt();
+        int numCities;
+        ArrayList<String> cities = new ArrayList<String>();
+        String city;
+        for (int i = 0; i < numTrips; i++) {
+            numCities = in.nextInt();
+            in.nextLine();
+            for (int j = 0; j < numCities; j++) {
+                city = in.nextLine();
+                if (!cities.contains(city)) cities.add(city);
+            }
+            System.out.println(cities.size());
+            cities.clear();
+        }
+    }
+}
