d6e4adebfc0963a438c4873e37af32a9f848f599
EchoEnzo <n8hynes@gmail.com>
2016-03-11 20:42:55 -0330

    Solving for Carrots + added samples/ dirs to .gitignore

---
+++ java/carrots/carrots.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class carrots {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        in.nextInt();
+        System.out.println(in.nextInt());
+    }
+}
