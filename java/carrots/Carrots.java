55ea51ef68063553a868b71b945dfe6209fcd672
EchoEnzo <n8hynes@gmail.com>
2016-03-14 11:24:52 -0230

    Change capitalization of files

--- java/carrots/Carrots.java
+++ java/carrots/Carrots.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class carrots {
+public class Carrots {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         in.nextInt();
--- java/everywhere/Everywhere.java
+++ java/everywhere/Everywhere.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class everywhere {
+public class Everywhere {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int numTrips = in.nextInt();
