8e5ce5deb5ace6f5aa6fe70373b2d4fe48273378
EchoEnzo <n8hynes@gmail.com>
2016-03-14 16:39:39 -0230

    'Hello World' typo fix

--- java/hello/Hello.java
+++ java/hello/Hello.java
@@ -2,6 +2,6 @@ import java.util.Scanner;
 public class Hello {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Hello World!")
+        System.out.println("Hello World!");
     }
 }
