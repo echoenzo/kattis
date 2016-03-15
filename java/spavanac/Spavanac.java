f323bad318a772a4d11a30c6ef7864a8318886df
EchoEnzo <n8hynes@gmail.com>
2016-03-14 23:48:58 -0230

    'Spavanac'

---
+++ java/spavanac/Spavanac.java
@@ -0,0 +1,153 @@
+// Template Author: Michael-G-Sullivan
+import java.io.*;
+import java.util.*;
+
+public class Spavanac {
+
+    FastScanner in;
+    PrintWriter out;
+    int i = 0, j = 0;
+
+    void solve() {
+        /**************START**************/
+
+        int h = in.nextInt();
+        int m = in.nextInt();
+
+        if (m < 45) {
+            m+=(60-45);
+            if (h == 0) {
+                h = 23;
+            }
+            else h-=1;
+        }
+        else m-=45;
+        out.println(h + " " + m);
+
+        /***************END***************/
+    }
+
+    public static void main(String[] args) {
+        new Spavanac().runIO();
+    }
+
+    void runIO() {
+        in = new FastScanner(System.in);
+        out = new PrintWriter(System.out);
+        solve();
+        out.close();
+    }
+
+    void printGrid(char[][] g) {
+        for (int a = 0; a < g.length; a++) {
+            out.println(new String(g[a]));
+        }
+    }
+
+    class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastScanner(File f) {
+            try {
+                br = new BufferedReader(new FileReader(f));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public FastScanner(InputStream f) {
+            br = new BufferedReader(new InputStreamReader(f));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                String s = null;
+                try {
+                    s = br.readLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (s == null)
+                    return null;
+                st = new StringTokenizer(s);
+            }
+            return st.nextToken();
+        }
+
+        //This will empty out the current line (if non-empty) and return the next line down. If the next line is empty, will return the empty string.
+        //If there is no more input, this will return null.
+        String nextLine() {
+            st = null;
+            String s = null;
+            try {
+                s = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return s;
+        }
+
+        boolean hasMoreTokens() {
+            while (st == null || !st.hasMoreTokens()) {
+                String s = null;
+                try {
+                    s = br.readLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (s == null)
+                    return false;
+                st = new StringTokenizer(s);
+            }
+            return true;
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        double[] getDoubleArray(int s) {
+            double[] arr = new double[s];
+            for (int d = 0; d < s; d++) {
+                arr[d] = in.nextDouble();
+            }
+            return arr;
+        }
+        int[] getIntArray(int s) {
+            int[] arr = new int[s];
+            for (int d = 0; d < s; d++) {
+                arr[d] = in.nextInt();
+            }
+            return arr;
+        }
+        char[][] getCharGrid() {
+            ArrayList<char[]> lines = new ArrayList<char[]>();
+            String line = in.nextLine();
+            while (line != null && line.length() == 0) {
+                line = in.nextLine();
+            }
+            if (line == null) {
+                return null;
+            }
+            while (line != null && line.length() > 0) {
+                lines.add(line.toCharArray());
+                line = in.nextLine();
+            }
+            char[][] grid = new char[lines.size()][];
+            for (int l = 0; l < grid.length; l++) {
+                grid[l] = lines.get(l);
+            }
+            return grid;
+
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+    }
+}
