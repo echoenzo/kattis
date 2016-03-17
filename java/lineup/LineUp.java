c844d7dbf526933874d9af94191e0b2af66c704b
EchoEnzo <n8hynes@gmail.com>
2016-03-17 13:34:36 -0230

    'Line Them Up'

---
+++ java/lineup/LineUp.java
@@ -0,0 +1,174 @@
+// Template Author: Michael-G-Sullivan
+// Author: echoenzo
+import java.io.*;
+import java.util.*;
+
+public class LineUp {
+
+    FastScanner in;
+    PrintWriter out;
+    int i = 0, j = 0;
+
+    void solve() {
+        /**************START**************/
+
+        int N = in.nextInt();
+        String s1 = in.next();
+        String s2 = in.next();
+        boolean inc = false;
+        boolean dec = false;
+        boolean nei = false;
+        while (s2 != null) {
+            if (s1.compareTo(s2) < 0) {
+                if (dec) {
+                    nei = true;
+                    dec = false;
+                    inc = false;
+                    break;
+                }
+                else inc = true;
+            }
+            else if (s1.compareTo(s2) > 0) {
+                if (inc) {
+                    nei = true;
+                    dec = false;
+                    inc = false;
+                    break;
+                }
+                else dec = true;
+            }
+            s1 = s2;
+            s2 = in.next();
+        }
+        if (nei) out.println("NEITHER");
+        else if (inc) out.println("INCREASING");
+        else if (dec) out.println("DECREASING");
+
+        /***************END***************/
+    }
+
+    public static void main(String[] args) {
+        new LineUp().runIO();
+    }
+
+    void runIO() {
+        in = new FastScanner(System.in);
+        out = new PrintWriter(System.out);
+        solve();
+        out.close();
+    }
+
+    void printGrid(char[][] g) {
+        for (int a = 0; a < g.length; a++) {
+            out.println(new String(g[a]));
+        }
+    }
+
+    class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastScanner(File f) {
+            try {
+                br = new BufferedReader(new FileReader(f));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public FastScanner(InputStream f) {
+            br = new BufferedReader(new InputStreamReader(f));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                String s = null;
+                try {
+                    s = br.readLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (s == null)
+                    return null;
+                st = new StringTokenizer(s);
+            }
+            return st.nextToken();
+        }
+
+        //This will empty out the current line (if non-empty) and return the next line down. If the next line is empty, will return the empty string.
+        //If there is no more input, this will return null.
+        String nextLine() {
+            st = null;
+            String s = null;
+            try {
+                s = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return s;
+        }
+
+        boolean hasMoreTokens() {
+            while (st == null || !st.hasMoreTokens()) {
+                String s = null;
+                try {
+                    s = br.readLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (s == null)
+                    return false;
+                st = new StringTokenizer(s);
+            }
+            return true;
+        }
+
+        double[] getDoubleArray(int s) {
+            double[] arr = new double[s];
+            for (int d = 0; d < s; d++) {
+                arr[d] = in.nextDouble();
+            }
+            return arr;
+        }
+
+        int[] getIntArray(int s) {
+            int[] arr = new int[s];
+            for (int d = 0; d < s; d++) {
+                arr[d] = in.nextInt();
+            }
+            return arr;
+        }
+
+        char[][] getCharGrid() {
+            ArrayList<char[]> lines = new ArrayList<char[]>();
+            String line = in.nextLine();
+            while (line != null && line.length() == 0) {
+                line = in.nextLine();
+            }
+            if (line == null) {
+                return null;
+            }
+            while (line != null && line.length() > 0) {
+                lines.add(line.toCharArray());
+                line = in.nextLine();
+            }
+            char[][] grid = new char[lines.size()][];
+            for (int l = 0; l < grid.length; l++) {
+                grid[l] = lines.get(l);
+            }
+            return grid;
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+    }
+}
